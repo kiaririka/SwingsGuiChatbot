f6e41dea5efd4fecb58ee44ad37755719f5c7844
kiaririka <isha79799@gmail.com>
2023-05-27 11:55:15 +0530

    Initial commit

---
+++ src/main/java/isha/project/ChatApplication.java
@@ -0,0 +1,113 @@
+package isha.project;
+
+import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class ChatApplication {
+    private JFrame frame;
+    private JTextArea chatArea;
+    private JTextField inputField;
+    private JPanel messagePanel;
+
+    public ChatApplication() {
+        frame = new JFrame("Chat Application");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(400, 500);
+        frame.setLayout(new BorderLayout());
+
+        // Chat area
+        chatArea = new JTextArea();
+        chatArea.setEditable(false);
+        chatArea.setLineWrap(true);
+        JScrollPane scrollPane = new JScrollPane(chatArea);
+        frame.add(scrollPane, BorderLayout.CENTER);
+
+        // Message input field
+        inputField = new JTextField();
+        inputField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String messageContent = inputField.getText();
+                processMessage(messageContent);
+                inputField.setText("");
+            }
+        });
+
+        // Send button
+        JButton sendButton = new JButton("Send");
+        sendButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                String messageContent = inputField.getText();
+                processMessage(messageContent);
+                inputField.setText("");
+            }
+        });
+
+        // Message input panel
+        JPanel inputPanel = new JPanel();
+        inputPanel.setLayout(new BorderLayout());
+        inputPanel.add(inputField, BorderLayout.CENTER);
+        inputPanel.add(sendButton, BorderLayout.EAST);
+        frame.add(inputPanel, BorderLayout.SOUTH);
+
+        // Message panel for user and bot messages
+        messagePanel = new JPanel();
+        messagePanel.setLayout(new BoxLayout(messagePanel, BoxLayout.Y_AXIS));
+        frame.add(messagePanel, BorderLayout.CENTER);
+
+        frame.setVisible(true);
+    }
+
+    private void processMessage(String messageContent) {
+        PostRequest request = new PostRequest();
+        String reply;
+        if (messageContent.equalsIgnoreCase("/about")) {
+            reply = "I am a chatbot made with the intent of helping anyone and everyone looking for a friend to talk to.\nFeel free to ask me anything or simply chat with me.";
+        } else if (messageContent.equalsIgnoreCase("/help")) {
+            reply = "Here are some options you can ask me:\n1. /about - To know more about me\n2. /help - To know more options to ask me\n3. /talk - To have a one-on-one talk session";
+        } else if (messageContent.equalsIgnoreCase("/talk")) {
+            reply = "Sure! Let's have a one-on-one talk.";
+        } else {
+            try {
+                reply = request.post(messageContent);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+
+        displayMessage("You: " + messageContent, true);
+        displayMessage("Bot: " + reply, false);
+    }
+
+    private void displayMessage(String message, boolean isUserMessage) {
+        JPanel messageWrapper = new JPanel();
+        messageWrapper.setLayout(new BorderLayout());
+        messageWrapper.setBorder(new EmptyBorder(5, 10, 5, 10));
+        JTextArea messageTextArea = new JTextArea(message);
+        messageTextArea.setEditable(false);
+        messageTextArea.setLineWrap(true);
+        messageTextArea.setWrapStyleWord(true);
+        messageTextArea.setBackground(isUserMessage ? Color.decode("#DCF8C6") : Color.decode("#E2E2E2"));
+        messageWrapper.add(messageTextArea, isUserMessage ? BorderLayout.WEST : BorderLayout.EAST);
+
+        messagePanel.add(messageWrapper);
+        messagePanel.revalidate();
+        messagePanel.repaint();
+
+        // Scroll to the bottom of the chat area
+        chatArea.setCaretPosition(chatArea.getDocument().getLength());
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                new ChatApplication();
+            }
+        });
+    }
+}
---
+++ src/main/java/isha/project/PostRequest.java
@@ -0,0 +1,36 @@
+package isha.project;
+
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import org.json.JSONObject;
+
+
+public class PostRequest {
+
+    public static String post(String message) throws Exception {
+        URL url = new URL("https://gpa-rho.vercel.app/getBard_Response");
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("POST");
+        connection.setRequestProperty("Content-Type", "application/json");
+        String requestBody = "{\"Query\": \"" + message + "\" ,\"Key\":\"sweetfriend\"}";
+        connection.setDoOutput(true);
+        connection.getOutputStream().write(requestBody.getBytes());
+
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+        String line;
+        String reply = null;
+        while ((line = bufferedReader.readLine()) != null) {
+
+            JSONObject jsonObject = new JSONObject(line);
+
+            reply = (String) jsonObject.get("Content");
+//            System.out.println(reply);
+        }
+        return reply;
+    }
+
+}
